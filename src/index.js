9f41b4d08ce358fe3b935801ebf6e241014ec5a0
woong0300 <woong0300@naver.com>
2020-03-04 21:55:31 +0900

    #01.Setting with create-react-app and Using simple concepts of Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+//Store란 state, data를 넣어놓기 위함
+//state는 app에서 변화가 있는 데이터를 일컫는다.
+//Redux는 데이터를 관리하는 것을 도와주기 위해 만들어졌다.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const plus = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//reducer, modifier란 데이터를 modify하고 그것을 return하는 함수다
+//그 데이터를 우리의 app의 있는 데이터이며 우리 app은 리턴데이터를 갖게됨.
+//우리의 데이터를 변형시킬 수 있는 유일한 파트를 몰아놨다고 생각.
+const countModifier = (count = 0, action) => {
+  if (action.type === "Add") {
+    console.log(count);
+    return count + 1;
+  } else if (action.type === "Minus") {
+    console.log(count);
+    return count - 1;
+  } else {
+    console.log(count);
+    return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+//redurcer에게 밖에서 전달하는 방법
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "Add" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "Minus" });
+};
+plus.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+// 이렇게 걍 handler없이 쓸 수 있다.
+// plus.addEventListener("click", () => countStore.dispatch({ type: "Add"}));
+// minus.addEventListener("click", () => countStore.dispatch({ type: "Minus"}));
+
+console.log(countStore.getState());
